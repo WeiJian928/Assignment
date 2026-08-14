866eb91b32541e04d76d4309a898b6c111c03f2a
siewfang07 <mahsf-pm25@student.tarc.edu.my>
2026-08-14 23:10:59 +0800

    Siew Fang test4

--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.assignment.ui.theme.AssignmentTheme
 
@@ -34,6 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        color = Color.Magenta,
         modifier = modifier
     )
 }
